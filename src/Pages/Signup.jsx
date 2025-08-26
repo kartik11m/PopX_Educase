1d8e25a4cdc30c0c9b188425ed334e92f9cb6c06
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-27 02:36:21 +0530

    minor fixes like changing color of the radio buttons

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -11,50 +11,50 @@ const Signup = () => {
                             Create your PopX Account
                         </h1>
                         <div>
-                            <div class="relative h-[40px] w-full mb-[29px]">
-                                <input id="full-name" type="text" placeholder='Full Name' class="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
-                                <label for="full-name-input" class="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
-                                    <span class="text-[#6C25FF]">Full Name</span>
-                                    <span class="text-[#DD4A3D]">*</span>
+                            <div className="relative h-[40px] w-full mb-[39px]">
+                                <input id="full-name" type="text" placeholder='Full Name' className="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
+                                <label for="full-name-input" className="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
+                                    <span className="text-[#6C25FF]">Full Name</span>
+                                    <span className="text-[#DD4A3D]">*</span>
                                 </label>
                             </div>
-                            <div class="relative h-[40px] w-full mb-[29px]">
-                                <input id="phone-number" type='number' placeholder='Phone number' class="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
-                                <label for="full-name-input" class="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
-                                    <span class="text-[#6C25FF]">Phone Number</span>
-                                    <span class="text-[#DD4A3D]">*</span>
+                            <div className="relative h-[40px] w-full mb-[39px]">
+                                <input id="phone-number" type='number' placeholder='Phone number' className="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
+                                <label for="full-name-input" className="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
+                                    <span className="text-[#6C25FF]">Phone Number</span>
+                                    <span className="text-[#DD4A3D]">*</span>
                                 </label>
                             </div>
-                            <div class="relative h-[40px] w-full mb-[29px]">
-                                <input id="email" type="email" placeholder='Email' class="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
-                                <label for="full-name-input" class="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
-                                    <span class="text-[#6C25FF]">Email address</span>
-                                    <span class="text-[#DD4A3D]">*</span>
+                            <div className="relative h-[40px] w-full mb-[39px]">
+                                <input id="email" type="email" placeholder='Email' className="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
+                                <label for="full-name-input" className="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
+                                    <span className="text-[#6C25FF]">Email address</span>
+                                    <span className="text-[#DD4A3D]">*</span>
                                 </label>
                             </div>
-                            <div class="relative h-[40px] w-full mb-[29px]">
-                                <input id="pass" type="password" placeholder='Password' class="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
-                                <label for="full-name-input" class="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
-                                    <span class="text-[#6C25FF]">Password</span>
-                                    <span class="text-[#DD4A3D]">*</span>
+                            <div className="relative h-[40px] w-full mb-[39px]">
+                                <input id="pass" type="password" placeholder='Password' className="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
+                                <label for="full-name-input" className="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
+                                    <span className="text-[#6C25FF]">Password</span>
+                                    <span className="text-[#DD4A3D]">*</span>
                                 </label>
                             </div>
-                            <div class="relative h-[40px] w-full mb-[18px]">
-                                <input id="company-name" type="text" placeholder='Company Name' class="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
-                                <label for="full-name" class="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
-                                    <span class="text-[#6C25FF]">Company Name</span>
+                            <div className="relative h-[40px] w-full mb-[30px]">
+                                <input id="company-name" type="text" placeholder='Company Name' className="w-full px-4 py-3 border border-[#CBCBCB] rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500" />
+                                <label for="full-name" className="absolute left-3 -top-2.5 px-1 text-[13px] bg-[#F7F8F9]">
+                                    <span className="text-[#6C25FF]">Company Name</span>
                                 </label>
                             </div>
                             <div>
-                                <p className='text-[13px]'>Are you an Agency? <span class="text-[#DD4A3D]">*</span></p>
+                                <p className='text-[13px]'>Are you an Agency? <span className="text-[#DD4A3D]">*</span></p>
                                 <div className='flex'>
-                                    <div class="flex items-center me-4">
-                                        <input type="radio" value="Yes" name="agency" class="w-4 h-4" />
-                                        <label for="radio" class="ms-2 text-[14px] font-normal">Yes</label>
+                                    <div className="flex items-center me-4">
+                                        <input type="radio" value="Yes" name="agency" className="w-4 h-4 accent-[#6C25FF]" />
+                                        <label for="radio" className="ms-2 text-[14px] font-normal">Yes</label>
                                     </div>
-                                    <div class="flex items-center me-4">
-                                        <input type="radio" value="No" name="agency" class="w-4 h-4" />
-                                        <label for="radio" class="ms-2 text-[14px] font-normal">No</label>
+                                    <div className="flex items-center me-4">
+                                        <input type="radio" value="No" name="agency" className="w-4 h-4 accent-[#6C25FF]" />
+                                        <label for="radio" className="ms-2 text-[14px] font-normal">No</label>
                                     </div>
                                 </div>
                             </div>
