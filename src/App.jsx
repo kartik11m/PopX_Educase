768e10ec4e08b27cb16e5e2739622ea43a96f62d
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-26 23:16:36 +0530

    Added routing to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from './Pages/Landing';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <>
-      Hello
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
