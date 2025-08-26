589d0a3e1a79261f5ef82f85a260ae811557cf29
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-26 23:53:53 +0530

    Added the layout for landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+        <div className="flex h-screen items-center justify-center bg-white">
+            <div className="relative flex h-[812px] w-[375px] flex-col rounded-lg border-gray-600 bg-[#F7F8F9] shadow-sm p-6">
+                <div className="flex flex-col items-start justify-end h-full w-full">
+                    <div>
+                        <h1 className="text-[28px] font-medium leading-[17px] text-[#1D2226] mb-2.5">
+                            Welcome to PopX
+                        </h1>
+                        <p className='mb-6 text-[#1D2226] font-normal text-[18px] leading-[26px] '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, reprehenderit?</p>
+                        <div>
+                            <Link to="/signup">
+                                <button className="w-full rounded-lg bg-[#6C25FF] py-2 text-[#FFFFFF] text-[16px] font-medium hover:bg-violet-800 transition mb-3">
+                                    Create Account
+                                </button>
+                            </Link>
+                            <Link to="/login">
+                                <button className="w-full rounded-lg bg-[#6C25FF4B] py-2 text-violet-900 font-medium text-[16px] hover:bg-blue-100 transition">
+                                    Already Registered?Login
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
