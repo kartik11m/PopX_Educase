617f472b6d58aa4690e1f92b175b15495e9ae8f9
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-27 02:07:09 +0530

    added the Group1 svg

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -8,8 +8,9 @@ const Profile = () => {
                 <p className='ms-3 font-medium text-[18px] text-[#1D2226]'>Account Setting</p>
             </div>
             <div className="flex h-screen w-[375px] flex-col gap-6 border border-[#0000000b] bg-[#F7F8F9] shadow-sm p-6">
-                <div className="flex items-start gap-5">
+                <div className="relative flex items-start gap-5">
                     <img className='h-20 w-20 rounded-full' src="/girl.png" alt="picture" />
+                    <img src="/Group1.svg" alt="" className='absolute h-[23px] top-13 left-[62px]'/>
                     <div>
                         <p className='text-[#1D2226] font-semibold text-[15px]'>Marry Doe</p>
                         <p className='text-[#1D2226] text-[14px]'>Marry@Gmail.com</p>
